837dc7a401f3655803d46a8d5c3d7d47bb6a0d39
Roman <igoshinr.estho@gmail.com>
2023-11-27 14:26:53 +0300

    Header&FooterComponents

---
+++ blog-vue/src/HeaderComp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="header">
+      <HeaderBlock/>
+    </div>
+</template>
+
+<script>
+import HeaderBlock from './components/HeaderBlock.vue'
+
+export default {
+  name: 'HeaderComp',
+  components: { HeaderBlock }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- blog-vue/src/main.js
+++ blog-vue/src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Header from './HeaderComp.vue'
+import Footer from './FooterComp.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+new Vue({
+  render: h => h(Header)
+}).$mount('#header')
+
+new Vue({
+  render: h => h(Footer)
+}).$mount('#footer')
